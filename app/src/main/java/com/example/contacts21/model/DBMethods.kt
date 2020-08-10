c572a20f4ec520d1509204327f06d6c8f8ff9000
dk <dk@cart.is>
2020-08-10 18:54:50 +0300

    d

--- app/src/main/java/com/example/contacts21/model/DBMethods.kt
+++ app/src/main/java/com/example/contacts21/model/DBMethods.kt
@@ -1,4 +1,15 @@
 package com.example.contacts21.model
 
-class DBMethods {
+class DBMethods(private val dbOpenHelper: DBOpenHelper) {
+    fun add(contact: Contact) {
+        dbOpenHelper.addContact(contact)
+    }
+
+    fun getContacts() {
+        val cursor = dbOpenHelper.getContact()
+    }
+
+    fun getContact(id: Int) {
+        val cursor = dbOpenHelper.getContact()
+    }
 }
--- app/src/main/java/com/example/contacts21/model/DBOpenHelper.kt
+++ app/src/main/java/com/example/contacts21/model/DBOpenHelper.kt
@@ -1,18 +1,49 @@
 package com.example.contacts21.model
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DBOpenHelper(context : Context, factory: SQLiteDatabase.CursorFactory?) :
     SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
 
-    override fun onCreate(p0: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+    override fun onCreate(db: SQLiteDatabase) {
+        val contactsTable = ("CREATE TABLE $TABLE_NAME($COLUMN_ID INTEGER PRIMARY KEY, $COLUMN_FIRSTNAME TEXT," +
+                "$COLUMN_LASTNAME TEXT, $COLUMN_MOBILE TEXT, $COLUMN_EMAIL TEXT)")
+        db.execSQL(contactsTable)
     }
 
-    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        TODO("Not yet implemented")
+    override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        onCreate(db)
+    }
+
+    fun addContact(contact : Contact) {
+        val values = ContentValues()
+        values.put(COLUMN_FIRSTNAME, contact.firstName)
+        values.put(COLUMN_LASTNAME, contact.lastName)
+        values.put(COLUMN_MOBILE, contact.mobile)
+        values.put(COLUMN_EMAIL, contact.email)
+        val db = this.writableDatabase
+        db.insert(TABLE_NAME, null, values)
+        db.close()
+    }
+
+    fun getContact() : Cursor? {
+        val db = this.readableDatabase
+        return db.rawQuery("SELECT * FROM $TABLE_NAME", null)
+    }
+
+    fun updateContact(contact: Contact) {
+        val values = ContentValues()
+        values.put(COLUMN_FIRSTNAME, contact.firstName)
+        values.put(COLUMN_LASTNAME, contact.lastName)
+        values.put(COLUMN_MOBILE, contact.mobile)
+        values.put(COLUMN_EMAIL, contact.email)
+        val db = this.writableDatabase
+        //db.update(TABLE_NAME, values, "_id = $id", null)
     }
 
     companion object {
